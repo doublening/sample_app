fd02c23d7110d20e5b46bacc49419c2049ae1193
Johnny <johnny@server>
2012-06-11 00:01:28 -0400

    User signup complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,11 +5,11 @@ describe UsersController do
  
   describe "GET 'new'" do
     it "returns http success" do
-      get 'new'
+      get :new
       response.should be_success
     end
   	it "should have the right title" do
-      get 'new'
+      get :new
       response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Sign up")
 	end
   end
@@ -43,4 +43,44 @@ describe UsersController do
 #      response.shouldhave_tag("h2>img", :class=> "gravatar")
 #    end	
 #  end  
+
+#  describe "POST 'create'"do
+#    describe "failure"do
+#      before(:each)do
+#        @attr= { :name=> "", :email=> "", :password=> "", :password_confirmation=> "" }
+#        @user=Factory.build(:user, @attr)
+#        User.stub!(:new).and_return(@user)
+#        @user.should_receive(:save).and_return(false)
+#      end
+
+#	  it "should have the right title"do
+#        post :create, :user=>@attr
+#        response.shouldhave_tag("title", /signup/i)
+#      end
+#
+#	  it "should render the 'new' page"do
+#        post :create, :user=>@attr
+#        response.should render_template('new')
+#      end
+#    end
+#     describe "success" do
+#       before(:each)do
+#         @attr= { :name=> "NewUser", :email=> "user@example.com",:password=> "foobar", :password_confirmation=> "foobar" }
+#         @user=Factory(:user, @attr)
+#         User.stub!(:new).and_return(@user)
+#         @user.should_receive(:save).and_return(true)
+#       end
+
+#	   it "should redirect to the user show page"do
+#         post :create, :user=>@attr
+#         response.should redirect_to(user_path(@user))
+#      end
+
+#      it "should have a welcome message"do
+#        post :create, :user=>@attr
+#        flash[:success].should =˜ /welcome to the sample app/i
+#      end
+#    end
+#  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'webrat'
+
+Webrat.configure do |config|
+  config.mode= :rails
+end
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
